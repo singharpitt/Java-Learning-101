fb3c9bd7d4e191e029cb742d4d967fd1626abe5f
Arpit Singh <arpit@Ubuntu22.04-Arpit>
2024-08-07 18:33:17 +0000

    first commit

---
+++ Basic Java/Day_01Hellomsg.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Day_01Hellomsg {
+    public static void main(String args[]){
+        System.out.println("Prints on the next line");
+        System.out.print("This gets to be printed on the same line !! \n"); 
+        System.err.print("This will get printed in red ,as this is a error!! \n");
+    }
+}
