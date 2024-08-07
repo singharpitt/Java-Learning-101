4a420b8a2cde9c09e43ff8ede64508afd8667220
Arpit Singh <arpit@Ubuntu22.04-Arpit>
2024-08-07 18:18:08 +0000

    Basic Java git status

---
+++ basic java/demo.java
@@ -0,0 +1,9 @@
+import java.util.*;
+class demo{
+    public static void main(String args []){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter No.-");
+        int a = sc.nextInt();
+        System.out.println(a+" Times Hello");
+    }
+}
